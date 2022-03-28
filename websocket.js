1529160b89e4ccec75113778c13e103eaa0fdb50
Wai Mun <waimun.ng@alpaca.markets>
2022-03-28 22:50:17 +0800

    Create websocket.js

---
+++ websocket.js
@@ -0,0 +1,68 @@
+"use strict";
+
+/**
+ * This examples shows how to use tha alpaca data v2 websocket to subscribe to events.
+ * You should use the alpaca api's data_steam_v2, also add feed besides the other parameters.
+ * For subscribing (and unsubscribing) to trades, quotes and bars you should call
+ * a function for each like below.
+ */
+
+const Alpaca = require("@alpacahq/alpaca-trade-api");
+const API_KEY = "";
+const API_SECRET = "";
+
+class DataStream {
+    constructor({ apiKey, secretKey }) {
+        this.alpaca = new Alpaca({
+            keyId: apiKey,
+            secretKey,
+            paper: true,
+        });
+
+        const socket = this.alpaca.data_stream_v2;
+
+        socket.onConnect(function () {
+            console.log("Connected");
+            socket.subscribeForQuotes(["AAPL"]);
+            socket.subscribeForTrades(["FB"]);
+            socket.subscribeForBars(["SPY"]);
+            socket.subscribeForStatuses(["*"]);
+        });
+
+        socket.onError((err) => {
+            console.log(err);
+        });
+
+        socket.onStockTrade((trade) => {
+            console.log(trade);
+        });
+
+        socket.onStockQuote((quote) => {
+            console.log(quote);
+        });
+
+        socket.onStockBar((bar) => {
+            console.log(bar);
+        });
+
+        socket.onStatuses((s) => {
+            console.log(s);
+        });
+
+        socket.onStateChange((state) => {
+            console.log(state);
+        });
+
+        socket.onDisconnect(() => {
+            console.log("Disconnected");
+        });
+
+        socket.connect();
+
+    }
+}
+
+let stream = new DataStream({
+    apiKey: API_KEY,
+    secretKey: API_SECRET,
+});
