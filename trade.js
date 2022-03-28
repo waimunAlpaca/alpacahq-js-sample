7efd04cdd936b1a3749cfac298b86ef493b1c4ef
Wai Mun <waimun.ng@alpaca.markets>
2022-03-28 22:50:21 +0800

    Create trade.js

---
+++ trade.js
@@ -0,0 +1,34 @@
+const Alpaca = require('@alpacahq/alpaca-trade-api')
+
+const API_KEY = '';
+const API_SECRET = '';
+const USE_POLYGON = false;  // by default we use the Alpaca data stream but you can change that
+
+class Trade {
+    constructor({ keyId, secretKey, paper = true }) {
+        this.alpaca = new Alpaca({
+            keyId: keyId,
+            secretKey: secretKey,
+            paper: paper,
+            usePolygon: USE_POLYGON
+        })
+
+        let stocks = ['', '']
+        this.long = []
+        this.short = []
+    }
+
+    async run() {
+        this.alpaca.getAccount().then((account) => {
+            console.log('Current Account:', account)
+        })
+    }
+}
+
+let ls = new Trade({
+    keyId: API_KEY,
+    secretKey: API_SECRET,
+    paper: true
+})
+
+ls.run()
